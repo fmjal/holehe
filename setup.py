a336dbe17d050ec85f27a561ff5a646ec9e761ed
user <user@host>
2024-07-22 17:28:48 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='holehe',
-    version="1.61.3",
+    version="1.61.4",
     packages=find_packages(),
     author="megadose",
     author_email="megadose@protonmail.com",
